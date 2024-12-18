0c2fe8dcb1dd0f89ebc4dfd072c5f4e0bbc698ab
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-12-18 09:53:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function setup(){
-    let buttons = document.getElementById("buttons");
-    let ok = document.createElement("button");
-    ok.innerHTML = "ok";
-    buttons.appendChild(ok);
-    let cancel = document.createElement("button");
-    cancel.innerHTML = "cancel";
-    buttons.appendChild(cancel);
-    alert("did this work");
-}
+
