9c6dfa244a5c4d9f5f21eba033b92cf71a635b19
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-12-18 16:54:47 +0000

    E-Block

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function setup(){
     let buttons = document.getElementById("buttons");
     let ok = document.createElement("button");
-    ok.innerHTML = "ok";
-    ok.addEventListener("click", getPassword);
+    ok.innerHTML="ok";
+    ok.addEventListener("click",checkPassword)
     buttons.appendChild(ok);
     let cancel = document.createElement("button");
-    cancel.innerHTML = "cancel";
+    cancel.innerHTML="cancel";
     buttons.appendChild(cancel);
 }
 
-function getPassword(){
+function checkPassword(){
     let dialogs = document.getElementById("dialogs");
-    let username = dialogs.querySelector(':nth-child(2)').value;
+    let userName = dialogs.querySelector(':nth-child(2)').value;
     let password = dialogs.querySelector(':nth-child(4)').value;
-    alert(username + ", " + password);
 
 }
